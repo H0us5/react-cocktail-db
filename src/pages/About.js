abf5f1b0c0e6b4c112da5d2bb6b663db8d962224
Bohdan Dovhal <46327995+H0us5@users.noreply.github.com>
2023-07-15 18:56:38 +0300

    init

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto ipsum
+        perferendis, sit asperiores consequuntur dolorem at temporibus placeat
+        magni animi minus quis ipsa repellat vel? Doloremque, nostrum placeat
+        exercitationem veritatis beatae accusamus ullam voluptates laboriosam,
+        cumque quod est fuga nihil.
+      </p>
+    </section>
+  );
+};
+
+export default About;
